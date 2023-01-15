72b20d2beb8b44c977b6f245a298024f603131ca
Wijayaac <wijayaac25@gmail.com>
2023-01-15 10:31:27 +0800

    Add : post , comment and author page

---
+++ src/components/Comment.vue
@@ -0,0 +1,11 @@
+<script setup>
+defineProps(["comments"]);
+</script>
+<template>
+  <div>
+    <div v-for="comment in comments" :key="comment.id">
+      <h4>{{ comment.name }}</h4>
+      <p>{{ comment.body }}</p>
+    </div>
+  </div>
+</template>
